55afee7b4c2a2d2ffda42905b8a4ff0f1add8a9d
Daniel Brotz <daniel@brotz.cz>
2022-03-30 11:57:55 +0200

    Add data.gov.cz data source

--- collectors/main.py
+++ collectors/main.py
@@ -4,3 +4,8 @@ if not 'EXCLUDE_WORLDBANK' in os.environ:
     import src.worldbank
 else:
     print('Skipping World Bank')
+
+if not 'EXCLUDE_DATAGOVCZ' in os.environ:
+    import src.datagovcz
+else:
+    print('Skipping data.gov.cz')
